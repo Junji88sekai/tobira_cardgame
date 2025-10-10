e17d92e0a3d7dfb2d6801691a98f7233271f7696
Junji88sekai <junyamasaki.es@gmail.com>
2025-10-10 08:59:02 +0200

    Update data.js

---
+++ data.js
@@ -0,0 +1,29 @@
+const cardData = [
+  {
+    lesson: 1,
+    topic: 'mukashibanashi_yomi', // 「読み物」データ
+    words: [
+      { jp: 'むかしむかし', es: 'Érase una vez' },
+      { jp: 'おじいさん', es: 'Anciano' },
+      { jp: 'おばあさん', es: 'Anciana' }
+    ]
+  },
+  {
+    lesson: 1,
+    topic: 'mukashibanashi_kaiwa', // 「会話」データ
+    words: [
+      { jp: 'こんにちは', es: 'Hola' },
+      { jp: 'ありがとう', es: 'Gracias' },
+      { jp: 'さようなら', es: 'Adiós' }
+    ]
+  },
+  {
+    lesson: 2,
+    topic: 'shopping_yomi',
+    words: [
+      { jp: 'お店', es: 'Tienda' },
+      { jp: '買い物', es: 'Compras' }
+    ]
+  }
+  // --- 以下、第15課までのデータを同様の形式で追加していく ---
+];
